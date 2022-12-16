4fbf7ab87e1d72a202c88f061ee9c28214401964
Nikita <nikitosikrasnov@gmail.com>
2022-12-16 23:57:22 +0300

    chat demo

--- gotohome/settings.py
+++ gotohome/settings.py
@@ -17,6 +17,8 @@ INSTALLED_APPS = [
     'posts.apps.PostsConfig',
     'homepage.apps.HomepageConfig',
     'users.apps.UsersConfig',
+    'chat',
+    'channels',
     'grappelli',
     'tinymce',
     'sorl.thumbnail',
@@ -58,8 +60,17 @@ TEMPLATES = [
         },
     },
 ]
-
+ASGI_APPLICATION = "gotohome.asgi.application"
 WSGI_APPLICATION = 'gotohome.wsgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 DATABASES = {
     'default': {
