aecdff7221485e928331466371e30a690b59a80f
Nikita <nikitosikrasnov@gmail.com>
2022-12-24 00:29:39 +0300

    resolve CI/CD problems too

--- maps/views.py
+++ maps/views.py
@@ -1,11 +1,12 @@
 from django.contrib import messages
+from django.shortcuts import redirect, render
 from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.shortcuts import redirect, render
 from djeym.models import Placemark
 
 from gotohome.settings import EMAIL_HOST_USER
+
 from users.models import CustomUser
 
 
@@ -20,45 +21,33 @@ def map(request):
     return render(request, template_name)
 
 
-
-# SIMPLE EXAMPLE.
-# 1. Notify administrator of a new custom marker.
-# 2. Notify user about successful moderation of his marker.
-# Mail server for testings: $ python -m smtpd -n -c DebuggingServer localhost:1025
 @receiver(post_save, sender=Placemark)
 def notify_email(instance, **kwargs):
-    """Notify by email of a new custom marker."""
-
-    """
-    # May come in handy. (Может пригодится.)
-    title = instance.header  # (html)
-    description = instance.body  # (html)
-    image_url = instance.user_image.url
-    """
-    print(instance.header, kwargs)
-    # Notify administrator of a new custom marker.
     if instance.is_user_marker and not instance.is_sended_admin_email:
         subject = 'Text subject'
         message = 'Text message - Url: ' + \
-            'http(s)://your.domain/admin/djeym/placemark/{}/change/'.format(instance.pk)
-        from_email = EMAIL_HOST_USER  # or corporate email
-        staff_users = list(i['email'] for i in CustomUser.objects.filter(is_staff=True).values('email').distinct())
-        print(staff_users)
-        recipient_list = staff_users  # Your work email
+            'http(s)://your.domain/admin/djeym/placemark/{}/change/'.format(
+                instance.pk
+            )
+        from_email = EMAIL_HOST_USER
+        staff_users = list(
+            i['email'] for i in CustomUser.objects.filter(
+                is_staff=True
+            ).values('email').distinct()
+        )
+        recipient_list = staff_users
         send_mail(subject, message, from_email,
                   recipient_list, fail_silently=False)
 
-        # Required
         instance.is_sended_admin_email = True
         instance.save()
-    # Notify user about successful moderation of his marker.
-    elif instance.active and instance.is_user_marker and not instance.is_sended_user_email:
+    elif instance.active and instance.is_user_marker \
+            and not instance.is_sended_user_email:
         subject = 'Text subject'
         message = 'Text message'
-        from_email = EMAIL_HOST_USER  # Your work email
+        from_email = EMAIL_HOST_USER
         recipient_list = [instance.user_email]
         send_mail(subject, message, from_email,
                   recipient_list, fail_silently=False)
-        # Required
         instance.is_sended_user_email = True
         instance.save()
